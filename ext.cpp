e4fa4dc1688285b4171fa73b1f7427d4a547dc70
hiro <jinguang.tong@anu.edu.au>
2024-05-31 00:39:42 +1000

    add render_equation functions

--- ext.cpp
+++ ext.cpp
@@ -3,17 +3,21 @@
  * GRAPHDECO research group, https://team.inria.fr/graphdeco
  * All rights reserved.
  *
- * This software is free for non-commercial, research and evaluation use 
+ * This software is free for non-commercial, research and evaluation use
  * under the terms of the LICENSE.md file.
  *
  * For inquiries contact  george.drettakis@inria.fr
  */
 
-#include <torch/extension.h>
 #include "rasterize_points.h"
+#include "render_equation.h"
+#include <torch/extension.h>
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("rasterize_gaussians", &RasterizeGaussiansCUDA);
   m.def("rasterize_gaussians_backward", &RasterizeGaussiansBackwardCUDA);
   m.def("mark_visible", &markVisible);
+  m.def("render_equation_forward", &RenderEquationForwardCUDA);
+  m.def("render_equation_backward", &RenderEquationBackwardCUDA);
+  m.def("render_equation_forward_complex", &RenderEquationForwardCUDA_complex);
 }
